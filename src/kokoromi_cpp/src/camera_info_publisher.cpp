2ddaefa54bb1d1aa63c441513af7b4267cb54e5d
borednuna <hanunshaka02@gmail.com>
2024-01-19 17:32:13 +0700

    feat: assigned roi values to camera info publisher

--- src/kokoromi_cpp/src/camera_info_publisher.cpp
+++ src/kokoromi_cpp/src/camera_info_publisher.cpp
@@ -25,6 +25,7 @@
 #include "sensor_msgs/msg/camera_info.hpp"
 
 #include "ninshiki_interfaces/msg/detected_objects.hpp"
+#include <rclcpp/executors/multi_threaded_executor.hpp>
 
 using DetectedObjects = ninshiki_interfaces::msg::DetectedObjects;
 using namespace std::chrono_literals;
@@ -41,7 +42,7 @@ public:
   MinimalPublisher()
   : Node("minimal_publisher"), count_(0)
   {
-    loadCameraMatrices("/home/nuna/ipm-ws/src/kokoromi_cpp/data/ConversionResult.json");
+    loadCameraMatrices("/home/hanun/ipm-ws/src/kokoromi_cpp/data/ConversionResult.json");
     publisher_ = this->create_publisher<sensor_msgs::msg::CameraInfo>("camera_info", 10);
     timer_ = this->create_wall_timer(
       500ms, std::bind(&MinimalPublisher::timer_callback, this));
@@ -52,9 +53,33 @@ private:
   {
     auto camera_info_msg = std::make_shared<sensor_msgs::msg::CameraInfo>();
     camera_info_msg->header.frame_id = "base_footprint";
-    camera_info_msg->height = 360;
+    camera_info_msg->height = 480;
     camera_info_msg->width = 640;
     camera_info_msg->distortion_model = "plumb_bob";
+
+    int left = 0;
+    int right = 0;
+    int top = 0;
+    int bottom = 0;
+
+    for (const auto& detected_object : detected_objects->detected_objects)
+    {
+      if (detected_object.label == "ball") {
+        std::cout << detected_object.label << std::endl;
+        std::cout << detected_object.score << std::endl;
+        std::cout << detected_object.left << std::endl;
+        std::cout << detected_object.top << std::endl;
+        std::cout << detected_object.right << std::endl;
+        std::cout << detected_object.bottom << std::endl;
+
+        left = detected_object.left * camera_info_msg->width;
+        right = detected_object.right * camera_info_msg->width;
+        top = detected_object.top * camera_info_msg->height;
+        bottom = detected_object.bottom * camera_info_msg->height;
+      }
+
+      break;
+    }
 
     // Placeholder values for distortion coefficients
     camera_info_msg->d = {distortion_coefficients_[0], distortion_coefficients_[1], distortion_coefficients_[2], distortion_coefficients_[3], distortion_coefficients_[4]};
@@ -67,6 +92,13 @@ private:
 
     // Placeholder values for projection matrix (3x4)
     camera_info_msg->p = {projection_matrix_[0], projection_matrix_[1], projection_matrix_[2], projection_matrix_[3], projection_matrix_[4], projection_matrix_[5], projection_matrix_[6], projection_matrix_[7], projection_matrix_[8], projection_matrix_[9], projection_matrix_[10], projection_matrix_[11]};
+
+    // Set ROI values
+    camera_info_msg->roi.x_offset = left;
+    camera_info_msg->roi.y_offset = top;
+    camera_info_msg->roi.height = bottom - top;
+    camera_info_msg->roi.width = right - left;
+    camera_info_msg->roi.do_rectify = false;
 
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", camera_info_msg->header.frame_id.c_str());
 
@@ -118,14 +150,30 @@ public:
   : Node("detection_subscriber")
   {
     detection_subscriber = this->create_subscription<DetectedObjects>(
-      "dnn_detection", 10, std::bind(&DetectionSubscriber::topic_callback, this));
+      "ninshiki_cpp/dnn_detection", 10, [this](const DetectedObjects::SharedPtr msg) {
+        this->topic_callback(msg);
+      });
   }
 
 private:
-  void topic_callback(const DetectedObjects::SharedPtr msg) const
+  void topic_callback(const DetectedObjects::SharedPtr & msg) const
   {
-    detected_objects = msg;
-    RCLCPP_INFO(this->get_logger(), "Received");
+      detected_objects = msg;
+
+      // Assuming DetectedObjects has an array of DetectedObject
+      // for (const auto& detected_object : detected_objects->detected_objects)
+      // {
+      //     if (detected_object.label == "ball") {
+      //       std::cout << detected_object.label << std::endl;
+      //       std::cout << detected_object.score << std::endl;
+      //       std::cout << detected_object.left << std::endl;
+      //       std::cout << detected_object.top << std::endl;
+      //       std::cout << detected_object.right << std::endl;
+      //       std::cout << detected_object.bottom << std::endl;
+      //     }
+      // }
+
+      RCLCPP_INFO(this->get_logger(), "Received");
   }
   rclcpp::Subscription<DetectedObjects>::SharedPtr detection_subscriber;
 };
@@ -133,7 +181,21 @@ private:
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalPublisher>());
+
+  // Create instances of both MinimalPublisher and DetectionSubscriber
+  auto minimal_publisher = std::make_shared<MinimalPublisher>();
+  auto detection_subscriber = std::make_shared<DetectionSubscriber>();
+
+  // Create a multi-threaded executor
+  auto executor = std::make_shared<rclcpp::executors::MultiThreadedExecutor>();
+
+  // Add both nodes to the executor
+  executor->add_node(minimal_publisher);
+  executor->add_node(detection_subscriber);
+
+  // Spin all nodes using the executor
+  executor->spin();
+
   rclcpp::shutdown();
   return 0;
 }
